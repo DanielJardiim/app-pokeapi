3252c83a1edd040e9406fdf080663e5845f1a358
DanielJardiim <jn_dani@outlook.com>
2025-06-20 17:52:01 -0300

    feat: define rotas principais (home, detalhes, favoritos) com lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'details/:name',
+    loadComponent: () =>
+      import('./details/details.page').then((m) => m.DetailsPage),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./favorites/favorites.page').then((m) => m.FavoritesPage),
   },
 ];
