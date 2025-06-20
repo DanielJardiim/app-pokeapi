7f659c9061aa75bd9b995289dbb309355a096b7f
DanielJardiim <jn_dani@outlook.com>
2025-06-20 17:55:07 -0300

    chore: configura bootstrap da aplicação e registra ícones personalizados

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,32 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+
+// ✅ Registro dos ícones que serão usados
+import { addIcons } from 'ionicons';
+import { heart, heartOutline } from 'ionicons/icons';
+addIcons({
+  heart: heart,
+  'heart-outline': heartOutline,
+});
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideHttpClient(),
   ],
 });
